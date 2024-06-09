d98d74b9c84d072f50f70d765bbad6e54aa11c29
Dushyant Kumar <dushyant787898@gmail.com>
2024-06-10 00:08:38 +0530

    add route for job

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const PORT=4000;
 
+//import router
+const userRouter= require('./routes/userRoute');
+const jobRoute = require('./routes/jobRoute');
+
 const app=express();
 // use middleware
 app.use(express.json());
-//import router
-const userRouter= require('./routes/userRoute');
 
 //mount the router
 app.use('/v1/user',userRouter);
+app.use('/v1/job',jobRoute);
 
 const mongo= process.env.MONGO_URL;
 
