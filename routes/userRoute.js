c40330634be0f06162983dd90e7452821af46c6e
Dushyant Kumar <dushyant787898@gmail.com>
2024-06-09 22:20:57 +0530

    add bcrypt in code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./../models/User");
+const bcrypt = require('bcrypt');
 
 router.get("/", (req, res) => {
   res.json({
@@ -8,18 +9,28 @@ router.get("/", (req, res) => {
     active: true,
   });
 });
-
+// we will validation middleware
 router.post("/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
 
-    await newUser.save();
-    res.status(201).json({
-      message: "New Entry Created successfully",
-      success: true,
-      user: newUser,
+  try {
+  const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email: email });
+  
+  if(existingUser){
+    res.status(400).json({
+      message:"User All ready exist, Please enter another email"
     });
+  }
+  else{
+    const hashPassword = await bcrypt.hash(password,10);
+    const newUser = new User({ name, email, password:hashPassword });
+  await newUser.save();
+  res.status(201).json({
+    message: "New User Created successfully",
+    success: true,
+    user: newUser,
+  });
+  }
     //we need to handle case where email already exist
   } catch (error) {
     console.log(error);
@@ -33,23 +44,29 @@ router.post("/register", async (req, res) => {
 router.get("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email, password });
-    if (user) {
-      res.status(200).json({
-        success: true,
-        message: "User Login succssfully",
-        user: user,
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      if (existingUser.password == password) {
+        res.status(200).json({
+          success: true,
+          message: "User Login succssfully",
+          email: existingUser.email,
+        });
+      }
+      else{
+        res.status(400).json({
+          
+          message:"Password not match"
+        });
+      }
+    } else {
+      res.status(401).json({
+        
+        message: "No user found",
       });
     }
-    else{
-        res.status(401).json({
-            success:false,
-            message:"users Details wrong",
-
-        });
-    }
   } catch (error) {
-    console.error(erro);
+    console.error(error);
     res.status(500).json({
       success: false,
       message: error.message,
